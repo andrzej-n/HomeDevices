d1dd9d66d2614d33b2080e0289015a12b1c7425d
andrzej-n <andrzej1410@onet.pl>
2018-02-10 23:40:20 +0100

    Create Spring-Boot initial project

---
+++ src/main/java/com/andrzejn/HomeDevices/HomeDevicesApplication.java
@@ -0,0 +1,12 @@
+package com.andrzejn.HomeDevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeDevicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeDevicesApplication.class, args);
+	}
+}
